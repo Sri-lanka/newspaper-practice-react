eacfa869caa254f065520119e0ecf725402fe9e9
Sri-lanka <davemdr911@hotmail.com>
2024-09-25 09:51:29 -0500

    update desing

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,9 @@
-
-
-
-
-import TableNew from './pages/tableNew';
-import TableUser from './pages/tableUser';
-import TableCategory from './pages/tableCategory';
-import Index from './pages/index';
-
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-
-
-const routes = createBrowserRouter([
-  { path: '/', element: <Index /> },
-  { path: '/users', element: <TableUser /> },
-  { path: '/news', element: <TableNew /> },
-  {path: '/categories', element: <TableCategory />},
-])
+import Index from "./pages/index";
 
 function App() {
   return (
     <>
-      
-        <div>
-          <h1>App</h1>
-          <nav>
-            <ul>
-              <li><a href="/users">users</a></li>
-              <li><a href="/news">news</a></li>
-              <li><a href="/categories">categories</a></li>
-            </ul>
-          </nav>
-          <RouterProvider router={routes} />
-        </div>
-   
+   <Index />
     </>
   );
 }
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,148 @@
 import Button from '@mui/material/Button';
+import SearchIcon from '@mui/icons-material/Search';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Accordion from '@mui/material/Accordion';
+import Card from '@mui/material/Card';
+import Box from '@mui/material/Box';
 
+import Stack from '@mui/material/Stack';
+import Divider from '@mui/material/Divider';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import CoPresentIcon from '@mui/icons-material/CoPresent';
 export default function Index() {
-  return <Button variant="contained">Hello world</Button>;
+
+
+  return (
+    <>
+      <div className="grid grid-cols-5 grid-rows-5 gap-0">
+        <div className="col-span-5 bg-blue-500">
+          <div className="flex items-center p-4 rounded-md">
+            <SearchIcon fontSize='large' className="ml-4 mr-2" />
+            <TextField id="standard-basic" label="Search New" variant="standard" />
+            <Typography sx={{ mx: 'auto' }} variant='h4'>My Newspaper</Typography>
+            <Button variant="contained">Iniciar Sesión</Button>
+          </div>
+        </div>
+        <div className="col-span-5 row-span-3 row-start-2 bg-green-500">
+          <div>
+            <div>
+              <Accordion>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1-content"
+                >
+                  Deportes
+                </AccordionSummary>
+                <AccordionDetails>
+                  <Card variant="outlined" sx={{ maxWidth: 360 }}>
+                    <Box sx={{ p: 2 }}>
+                      <Stack
+                        direction="row"
+                        sx={{ justifyContent: 'space-between', alignItems: 'center' }}
+                      >
+                        <Typography gutterBottom variant="h5" component="div">
+                          titulo
+                        </Typography>
+                        <Typography gutterBottom variant="h6" component="div">
+                          usuario
+                          <CoPresentIcon></CoPresentIcon>
+                        </Typography>
+                      </Stack>
+                      <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+                        Pinstriped cornflower blue cotton blouse takes you on a walk to the park or
+                        just down the hall.
+                      </Typography>
+                    </Box>
+                  </Card>
+                </AccordionDetails>
+              </Accordion>
+            </div>
+            <div>
+              <Accordion sx={{ bgcolor: 'red', color: 'white' }}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1-content"
+                  id="panel1-header"
+                >
+                  Categories
+                </AccordionSummary>
+                <AccordionDetails>
+                  <a>News</a><br />
+                  <a>Sports</a><br />
+                  <a>Technology</a><br />
+                  <a>Entertainment</a><br />
+                  <a>Politics</a><br />
+                </AccordionDetails>
+              </Accordion>
+            </div>
+            <div>
+              <Accordion sx={{ bgcolor: 'red', color: 'white' }}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1-content"
+                  id="panel1-header"
+                >
+                  Categories
+                </AccordionSummary>
+                <AccordionDetails>
+                  <a>News</a><br />
+                  <a>Sports</a><br />
+                  <a>Technology</a><br />
+                  <a>Entertainment</a><br />
+                  <a>Politics</a><br />
+                </AccordionDetails>
+              </Accordion>
+            </div>
+            <div>
+              <Accordion sx={{ bgcolor: 'red', color: 'white' }}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1-content"
+                  id="panel1-header"
+                >
+                  Categories
+                </AccordionSummary>
+                <AccordionDetails>
+                  <a>News</a><br />
+                  <a>Sports</a><br />
+                  <a>Technology</a><br />
+                  <a>Entertainment</a><br />
+                  <a>Politics</a><br />
+                </AccordionDetails>
+              </Accordion>
+            </div>
+            <div>
+            <Accordion sx={{ bgcolor: 'red', color: 'white' }}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1-content"
+                id="panel1-header"
+              >
+                Categories
+              </AccordionSummary>
+              <AccordionDetails>
+                <a>News</a><br />
+                <a>Sports</a><br />
+                <a>Technology</a><br />
+                <a>Entertainment</a><br />
+                <a>Politics</a><br />
+              </AccordionDetails>
+            </Accordion>
+          </div>
+          </div>
+        </div>
+        <div className="col-span-5 row-start-5 bg-yellow-500">
+          <div class="container" className='items-center'>
+            <p>&copy; 2024 Mi Periódico. Todos los derechos reservados.</p>
+          </div>
+        </div>
+      </div>
+
+    </>
+  );
 }
+
+
